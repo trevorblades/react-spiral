e1baf32d71ec34e137a82e409a6b7e0fa519b351
Trevor Blades <tdblades@gmail.com>
2021-05-09 23:16:18 -0700

    Resize shape to fit bounding box

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ shoutout:
 - https://educationisaround.com/sohcahtoa/
 - right triangle with two equal sides: http://www.math.com/school/subject3/lessons/S3U3L4DP.html#:~:text=A%20right%20triangle%20can%20also,that%20is%20an%20isosceles%20triangle.
 - bounding box of ngon: http://calcresource.com/geom-ngon.html
+- law of cosines: https://courses.lumenlearning.com/suny-osalgebratrig/chapter/non-right-triangles-law-of-cosines/
+
+> The Law of Cosines states that the square of any side of a triangle is equal to the sum of the squares of the other two sides minus twice the product of the other two sides and the cosine of the included angle.
--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+function getSideLength(width, sides) {
+  const centralAngle = (Math.PI * 2) / sides;
+  if (sides % 2) {
+    // odd number of sides
+    const circumradius =
+      width / Math.sin(((sides - 1) / (sides * 2)) * Math.PI) / 2;
+    const b2 = circumradius ** 2 * 2;
+    return Math.sqrt(b2 - b2 * Math.cos(centralAngle));
+  }
+
+  // use different ratio depending if N/2 is odd or even
+  const ratio = (sides / 2) % 2 ? Math.sin : Math.tan;
+  return (width / 2) * (ratio(centralAngle / 2) * 2);
+}
+
 function Spiral({sides, width, spacing, segments}) {
+  // shoutout
+  // https://www.mathsisfun.com/geometry/interior-angles-polygons.html
+  // https://educationisaround.com/sohcahtoa/
+  // right triangle with two equal sides: http://www.math.com/school/subject3/lessons/S3U3L4DP.html#:~:text=A%20right%20triangle%20can%20also,that%20is%20an%20isosceles%20triangle.
+  // bounding box of ngon: http://calcresource.com/geom-ngon.html
   const sumAngles = (sides - 2) * 180;
   const eachAngle = sumAngles / sides;
   const inset = Math.cos(eachAngle * (Math.PI / 180)) * spacing * 2;
+
+  const sideLength = getSideLength(width, sides);
+  console.log(sideLength);
   return (
     <div
       style={{
         width,
-        height: width
+        height: width,
+        padding: `0 ${(width - sideLength) / 2}px`,
+        overflow: 'hidden'
       }}
     >
       {segments
@@ -23,7 +48,7 @@ function Spiral({sides, width, spacing, segments}) {
           const behind = Math.max(Math.floor((offset - 2) / sides), 0);
           const combined = ahead + behind;
 
-          const innerWidth = width - combined * spacing - inset * middle;
+          const innerWidth = sideLength - combined * spacing - inset * middle;
           if (innerWidth < spacing) {
             return null;
           }
