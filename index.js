3fdbffb6f8c9936a4c5213aa6ff9b1c17358a0ed
Trevor Blades <tdblades@gmail.com>
2021-05-10 23:33:16 -0700

    Reorganize and polish sizing/spacing

--- index.js
+++ index.js
@@ -1,67 +1,93 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+function getFeatures(size, sides, centralAngle) {
+  if (sides % 2) {
+    // odd number of sides
+    const circumradius =
+      size / Math.sin(((sides - 1) / (sides * 2)) * Math.PI) / 2;
+    const inradius = Math.cos(centralAngle / 2) * circumradius;
+    const a = circumradius ** 2 * 2;
+    return {
+      sideLength: Math.sqrt(a - a * Math.cos(centralAngle)),
+      height: inradius + circumradius
+    };
+  }
+
+  const radius = size / 2;
+  const isHalfOdd = (sides / 2) % 2;
+  const ratio = isHalfOdd ? Math.sin : Math.tan;
+  return {
+    sideLength: radius * (ratio(centralAngle / 2) * 2),
+    height: (isHalfOdd ? Math.cos(centralAngle / 2) * radius : radius) * 2
+  };
+}
+
 function Spiral({size, sides, spacing, segments}) {
   const centralAngle = (Math.PI * 2) / sides;
-  const circumradius = size / 2;
-  const sideLength = circumradius * Math.sin(centralAngle / 2) * 2;
-  const inset = Math.cos(Math.PI - centralAngle) * spacing * 2;
-  const inradius = Math.cos(centralAngle / 2) * circumradius;
-  const height = sides % 2 ? inradius + circumradius : inradius * 2;
+  const {sideLength, height} = getFeatures(size, sides, centralAngle);
+  const interiorAngle = Math.PI - centralAngle;
+  const inset = Math.cos(interiorAngle) * spacing * 2;
   const spacingRatio = spacing / size;
   return (
     <div
       style={{
-        width: size,
-        height: size,
-        paddingTop: (size - height) / 2,
-        paddingLeft: (size - sideLength) / 2,
-        overflow: 'hidden'
+        overflow: 'hidden',
+        padding: '0.5em'
       }}
     >
-      {segments
-        .slice()
-        .reverse()
-        .reduce((child, value, index, array) => {
-          const side = array.length - index;
-          const [a, b, c] = Array.from({length: 3}, (value, index) =>
-            Math.max(Math.floor((side - index) / sides), 0)
-          );
-          const innerWidth = sideLength - (a + c) * spacing - inset * b;
+      <div
+        style={{
+          width: size,
+          height: size,
+          paddingTop: (size - height) / 2,
+          paddingLeft: (size - sideLength) / 2
+        }}
+      >
+        {segments
+          .slice()
+          .reverse()
+          .reduce((child, value, index, array) => {
+            const side = array.length - index;
+            const [a, b, c] = Array.from({length: 3}, (value, index) =>
+              Math.max(Math.floor((side - index) / sides), 0)
+            );
+            const innerWidth = sideLength - (a + c) * spacing - inset * b;
 
-          if (innerWidth < spacing) {
-            return null;
-          }
+            if (innerWidth < spacing) {
+              return null;
+            }
 
-          return (
-            <div
-              style={{
-                display: 'flex',
-                transformOrigin: 'left',
-                transform:
-                  side > 1
-                    ? `rotate(${centralAngle * (180 / Math.PI)}deg)`
-                    : 'translateY(-50%)'
-              }}
-            >
-              <span
+            return (
+              <div
                 style={{
-                  flexShrink: 0,
                   display: 'flex',
-                  whiteSpace: 'pre',
-                  justifyContent: 'space-evenly',
-                  width: innerWidth,
-                  padding: `0 ${(innerWidth * spacingRatio) / 2}px`
+                  transformOrigin: 'left',
+                  transform:
+                    side > 1
+                      ? `rotate(${centralAngle * (180 / Math.PI)}deg)`
+                      : 'translateY(-50%)'
                 }}
               >
-                {value.split('').map((char, index) => (
-                  <span key={index}>{char}</span>
-                ))}
-              </span>
-              {child}
-            </div>
-          );
-        }, null)}
+                <span
+                  style={{
+                    flexShrink: 0,
+                    display: 'flex',
+                    whiteSpace: 'pre',
+                    justifyContent: 'space-evenly',
+                    width: innerWidth,
+                    padding: `0 ${(innerWidth * spacingRatio) / 2}px`
+                  }}
+                >
+                  {value.split('').map((char, index) => (
+                    <span key={index}>{char}</span>
+                  ))}
+                </span>
+                {child}
+              </div>
+            );
+          }, null)}
+      </div>
     </div>
   );
 }
