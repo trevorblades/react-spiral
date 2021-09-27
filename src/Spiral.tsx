f0c0b372d4d7d2e989434a93106431659c2b8844
Trevor Blades <tdblades@gmail.com>
2021-09-26 18:03:29 -0700

    Memoize everything

--- src/Spiral.tsx
+++ src/Spiral.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
 interface Measurements {
   height: number;
@@ -72,7 +72,7 @@ interface Segment {
 }
 
 interface SpiralProps {
-  boxSize: number;
+  size: number;
   fontSize: number;
   sides: number;
   spacing: number;
@@ -80,94 +80,114 @@ interface SpiralProps {
 }
 
 export function Spiral(props: SpiralProps): JSX.Element {
-  const {boxSize, fontSize, sides, spacing, children} = props;
-  const centralAngle = (Math.PI * 2) / sides;
+  const {size, fontSize, sides, spacing, children} = props;
+  const centralAngle = useMemo(() => (Math.PI * 2) / sides, [sides]);
 
-  const totalSize = boxSize - fontSize;
-  const {height, sideLength} = measureShape(totalSize, sides, centralAngle);
-  const [inset, outset] = calcInOutset(spacing, centralAngle);
+  const totalSize = useMemo(() => size - fontSize, [size, fontSize]);
+  const {height, sideLength} = useMemo(
+    () => measureShape(totalSize, sides, centralAngle),
+    [totalSize, sides, centralAngle]
+  );
 
-  const charWidth = fontSize / 1.5; // 0.5em - same as 1ch
-  const basePadding = charWidth / 2; // base padding is half a character width
+  const [inset, outset] = useMemo(() => calcInOutset(spacing, centralAngle), [
+    spacing,
+    centralAngle
+  ]);
 
-  // we give progressively less padding to polygons with more than 4 sides
-  const paddingModifier = 4 / sides;
-  const padding = basePadding * paddingModifier;
+  const [charWidth, basePadding] = useMemo(() => {
+    const charWidth = fontSize / 1.5;
+    return [
+      charWidth, // 0.5em - same as 1ch
+      charWidth / 2 // base padding is half a character width
+    ];
+  }, [fontSize]);
 
-  const segments: Segment[] = [];
+  const padding = useMemo(() => {
+    // we give progressively less padding to polygons with more than 4 sides
+    const paddingModifier = 4 / sides;
+    return basePadding * paddingModifier;
+  }, [basePadding, sides]);
 
   // replace all extra space and then split on spaces
-  const words = children
-    .trim() // trim leading/trailing space
-    .replace(/\s+/, ' ') // turn one or more consecutive spaces into one space
-    .split(' ') // split into array of words
-    .map(text => ({
-      text,
-      isFullWord: true // mark as full word
-    }));
+  const words = useMemo(
+    () =>
+      children
+        .trim() // trim leading/trailing space
+        .replace(/\s+/, ' ') // turn one or more consecutive spaces into one space
+        .split(' ') // split into array of words
+        .map(text => ({
+          text,
+          isFullWord: true // mark as full word
+        })),
+    [children]
+  );
 
-  while (sideLength > inset) {
-    const side = segments.length + 1;
+  const segments = useMemo(() => {
+    const segments: Segment[] = [];
+    while (sideLength > inset) {
+      const side = segments.length + 1;
 
-    const [numInsets, numOutsets] = getNumInOutsets(side, sides);
-    const outerWidth = sideLength - inset * numInsets - outset * numOutsets;
+      const [numInsets, numOutsets] = getNumInOutsets(side, sides);
+      const outerWidth = sideLength - inset * numInsets - outset * numOutsets;
 
-    if (outerWidth < inset) {
-      break;
-    }
-
-    // account for padding on either side of the segment
-    const innerWidth = outerWidth - padding * 2;
-
-    // calculate the number of characters that can fit in the segment
-    let numChars = Math.floor(innerWidth / charWidth);
-    if (numChars <= 0) {
-      break;
-    }
-
-    // fill the segment with text until all characters have been accounted for
-    let text = '';
-    while (numChars > 0) {
-      // grab the first word
-      const word = words.shift();
-
-      // return it to the end of the stack if it's a full word
-      if (word.isFullWord) {
-        words.push(word);
+      if (outerWidth < inset) {
+        break;
       }
 
-      // if there isn't enough space for the full word
-      if (word.text.length > numChars) {
-        // grab the part of it that will fit
-        const fragment = word.text.slice(0, numChars);
-        // and put the rest of it back into the front of the stack
-        words.unshift({
-          text: word.text.slice(numChars),
-          isFullWord: false // not a full word
-        });
-        text += fragment;
-        numChars -= fragment.length;
-      } else {
-        // otherwise add the word to the segment
-        text += word.text;
-        numChars -= word.text.length;
-        if (numChars > 1) {
-          // add a space if there's room
-          text += ' ';
-          numChars -= 1;
-        } else if (numChars) {
-          // otherwise bail out and complete the segment
-          break;
+      // account for padding on either side of the segment
+      const innerWidth = outerWidth - padding * 2;
+
+      // calculate the number of characters that can fit in the segment
+      let numChars = Math.floor(innerWidth / charWidth);
+      if (numChars <= 0) {
+        break;
+      }
+
+      // fill the segment with text until all characters have been accounted for
+      let text = '';
+      while (numChars > 0) {
+        // grab the first word
+        const word = words.shift();
+
+        // return it to the end of the stack if it's a full word
+        if (word.isFullWord) {
+          words.push(word);
+        }
+
+        // if there isn't enough space for the full word
+        if (word.text.length > numChars) {
+          // grab the part of it that will fit
+          const fragment = word.text.slice(0, numChars);
+          // and put the rest of it back into the front of the stack
+          words.unshift({
+            text: word.text.slice(numChars),
+            isFullWord: false // not a full word
+          });
+          text += fragment;
+          numChars -= fragment.length;
+        } else {
+          // otherwise add the word to the segment
+          text += word.text;
+          numChars -= word.text.length;
+          if (numChars > 1) {
+            // add a space if there's room
+            text += ' ';
+            numChars -= 1;
+          } else if (numChars) {
+            // otherwise bail out and complete the segment
+            break;
+          }
         }
       }
-    }
 
-    segments.unshift({
-      side,
-      text,
-      width: outerWidth
-    });
-  }
+      segments.unshift({
+        side,
+        text,
+        width: outerWidth
+      });
+    }
+    return segments;
+  }, [sideLength, charWidth, inset, outset, padding, sides, words]);
 
   return (
     <div
